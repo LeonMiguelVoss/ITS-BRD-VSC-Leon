554f69d65d58b2b1aefd4c82646676943bae1c67
Leon Miguel Voss <leon.m.voss@gmail.com>
2026-05-27 14:14:48 +0200

    Aufgabe 4.2

--- Programs/PrimzahlSiebASM/Prototyp java/PrimzahlSieb.java
+++ Programs/PrimzahlSiebASM/Prototyp java/PrimzahlSieb.java
@@ -1,32 +1,38 @@
 public class PrimzahlSieb {
-	public static void main(String[] args) {
-		boolean[] sieb = new boolean[1001];
-		
-		int i = 2;
-		
-		while (i <= 1000) {
-			sieb[i] = true;
-			++i;
-		}
-		i = 2;
-		while (i <= 1000) {
-			if (sieb[i] == true) {
-				int c = i + 1;
-				while (c <= 1000) {
-					if (c % i == 0) {
-						sieb[c] = false;
-					}
-					++c;
+    public static void main(String[] args) {
+
+        boolean[] sieb = new boolean[1001];
+
+        int i = 2;
+
+        // Alle Zahlen als prim markieren
+        while (i <= 1000) {
+            sieb[i] = true;
+            ++i;
+        }
+        i = 2;
+
+        // Sieb 
+        while (i <= 1000) {
+
+            if (sieb[i]) {
+
+                int t = i * i;
+				while (t <= 1000) {
+					sieb[t] = false;
+					t += i;
 				}
-			}
-			++i;
-		}
-		i = 2;
-		while (i <= 1000) {
-			if(sieb[i] == true) {
-				System.out.println(i);
-			}
-			++i;
-		}
-	}
+            }
+            ++i;
+        }
+
+        // Ausgabe
+        i = 2;
+        while (i <= 1000) {
+            if (sieb[i]) {
+                System.out.println(i);
+            }
+            ++i;
+        }
+    }
 }
